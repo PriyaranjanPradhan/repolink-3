adceacb5810be0caa902fcdcbb332f840fd46df5
PriyaranjanPradhan <31290221+PriyaranjanPradhan@users.noreply.github.com>
2017-09-16 00:14:38 +0530

    Add files via upload

--- ans65.cpp
+++ ans65.cpp
@@ -1,22 +1,28 @@
 #include<iostream>
 using namespace std;
  
-/*PROGRAM TO CONVERT BINARY TO HEXADECIMAL NUMBER SYSTEM*/
+/*PROGRAM TO CONVERT OCTAL TO HEXADECIMAL NUMBER SYSTEM*/
 int main()
 {
-    long bin, dec = 0, rem, num, base = 1;
-    cout<<"PROGRAM TO CONVERT BINARY TO HEXADECIMAL NUMBER SYSTEM";
-    cout << "\n \nEnter the binary number(1s and 0s) : ";
+    long int oct, dec = 0, rem, num, base = 1;
+    cout<<"PROGRAM TO CONVERT OCTAL TO HEXADECIMAL NUMBER SYSTEM";
+    cout << "\n \nEnter the octal number(combination of digits 0 to 7) : ";
     cin >> num;
-    bin = num;
+    oct = num;
+    
+    //CONVERTING OCTAL TO DECIMAL
+    
+    
     while (num > 0)
     {
         rem = num % 10;
         dec = dec + rem * base;
-        base = base * 2;
+        base = base * 8;
         num = num / 10;
     }
+    
     //CONVERTING DECIMAL TO HEXADECIMAL
+    
     long int quo;
     int i=1,j,temp;
     char hexa[100];
@@ -33,7 +39,7 @@ int main()
       hexa[i++]= temp;
       quo = quo / 16;
     }
-    cout<<"\nEquivalent hexadecimal number of "<<bin<<" is : ";
+    cout<<"\n \nEquivalent hexadecimal number of "<<oct<<" is : ";
     for(j = i -1 ;j> 0;j--)
     cout<<hexa[j];
     cout<<endl;
